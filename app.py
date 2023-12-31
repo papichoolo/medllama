32a1b878d507b9ade5b26b33ac939c71a8018f82
dutta <tmberooney@users.noreply.huggingface.co>
2023-12-31 07:57:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 config = PeftConfig.from_pretrained("tmberooney/medllama")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", use_auth_token=secret_key, llm_int8_enable_fp32_cpu_offload=True, torch_dtype=torch.float16)
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", use_auth_token=True, llm_int8_enable_fp32_cpu_offload=True, torch_dtype=torch.float16)
 model = PeftModel.from_pretrained(model, "tmberooney/medllama", use_auth_token=secret_key)
 tokenizer=AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 device_map = {"transformer.word_embeddings": "cpu",
